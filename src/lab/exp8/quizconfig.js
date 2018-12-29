54476c12a1415183cc43959473d7c0a455bf8476
prayuktha <44540185+prayuktha@users.noreply.github.com>
2018-12-29 23:39:39 +0530

    Update quizconfig.js

--- src/lab/exp8/quizconfig.js
+++ src/lab/exp8/quizconfig.js
@@ -11,7 +11,7 @@ var totalquestions=10
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
 correctchoices[1]='a' //question 1 solution
-correctchoices[2]='b' //question 2 solution, and so on.
+correctchoices[2]='b'; //question 2 solution, and so on.
 correctchoices[3]='b'
 correctchoices[4]='a';
 correctchoices[5]='c'
@@ -36,7 +36,7 @@ for (q=1;q<=totalquestions;q++){
 		if (incorrect==null)
 		incorrect=q
 		else
-		incorrect+="/"+q
+		incorrect+="/"+q;
 		}
 	}
 
@@ -55,7 +55,7 @@ var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
 win2.document.write('<title>Solution</title>');
-win2.document.write('<body bgcolor="#FFFFFF">')
+win2.document.write('<body bgcolor="#FFFFFF">');
 win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
 for (i=1;i<=totalquestions;i++){
